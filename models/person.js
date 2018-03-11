bd1f2d20ecc79f8c11ee05b89efe848be031b29d
mstop4 <mstop4@gmail.com>
2018-03-10 19:24:51 -0500

    setup sequelize

---
+++ models/person.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Person = sequelize.define('Person', {
+    name: DataTypes.STRING
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Person;
+};
